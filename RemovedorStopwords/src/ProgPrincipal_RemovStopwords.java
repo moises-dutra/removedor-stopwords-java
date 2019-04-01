3fa21ba9846e04a36b88e015e47b0cc78dbc3a2a
moises-dutra <31829710+moises-dutra@users.noreply.github.com>
2019-04-01 14:20:28 -0300

    Update ProgPrincipal_RemovStopwords.java

--- RemovedorStopwords/src/ProgPrincipal_RemovStopwords.java
+++ RemovedorStopwords/src/ProgPrincipal_RemovStopwords.java
@@ -8,7 +8,7 @@ public class ProgPrincipal_RemovStopwords {
 	public static void main(String[] args) {
 
 		String arquivoStopwords = "    "; //inclua aqui o caminho para o arquivo com a lista de stopwords 
-		String arquivoCorpus = "    "; //inclua aqui o caminho para o arquivo com o corpus textual 
+		String arquivoCorpus = "    "; //inclua aqui o caminho para o arquivo com o corpus textual LIMPO e TOKENIZADO
 
 		Removedor removedor = new Removedor(arquivoStopwords, arquivoCorpus);
 		removedor.removerStopWords();
