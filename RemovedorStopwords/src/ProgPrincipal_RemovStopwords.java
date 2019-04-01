fc03dfb3d0591865fe45c6cdac43ae2f60a1f96a
moises-dutra <31829710+moises-dutra@users.noreply.github.com>
2019-04-01 13:54:58 -0300

    Add files via upload

---
+++ RemovedorStopwords/src/ProgPrincipal_RemovStopwords.java
@@ -0,0 +1,17 @@
+
+public class ProgPrincipal_RemovStopwords {
+
+	public ProgPrincipal_RemovStopwords() {
+		
+	}
+
+	public static void main(String[] args) {
+
+		String arquivoStopwords = "    "; //inclua aqui o caminho para o arquivo com a lista de stopwords 
+		String arquivoCorpus = "    "; //inclua aqui o caminho para o arquivo com o corpus textual 
+
+		Removedor removedor = new Removedor(arquivoStopwords, arquivoCorpus);
+		removedor.removerStopWords();
+	}
+
+}
---
+++ RemovedorStopwords/src/Removedor.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Removedor {
+
+	private String textoCorpus = "";
+	private String nomeArquivoStopwords = "";
+	private String nomeArquivoCorpus = "";
+	
+	private List<String> stopwords = null; 
+
+	public Removedor(String arquivoStopwords, String arquivoCorpus) {
+
+		nomeArquivoStopwords = arquivoStopwords;
+		nomeArquivoCorpus = arquivoCorpus;
+
+		try {
+			
+			stopwords = Files.readAllLines(new File(nomeArquivoStopwords).toPath(), Charset.defaultCharset() );
+			
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	
+		try {
+			
+			textoCorpus = new String(Files.readAllBytes(Paths.get(nomeArquivoCorpus)));
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	public void removerStopWords() {
+
+
+		String stopWordsPattern = String.join("|", stopwords);
+		Pattern pattern = Pattern.compile("\\b(?:" + stopWordsPattern + ")\\b\\s*", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(textoCorpus);
+
+		textoCorpus = matcher.replaceAll("");
+
+		System.out.print(textoCorpus);
+	}
+
+}
